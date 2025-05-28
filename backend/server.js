4397b94798da0773b4005bf5d1a3a6885099be2e
Creator Rama <ap410485@gmail.com>
2025-05-28 07:44:41 +0530

    lk

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-  origin:['https://eclypse-eight.vercel.app/','http://localhost:5173']
+  origin:['https://eclypse-eight.vercel.app','http://localhost:5173']
 }));
 app.use(express.json());
 app.use(express.static('public'));
