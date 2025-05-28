98e63e794aab069abcbe8e204a013b28a65d8416
Creator Rama <ap410485@gmail.com>
2025-05-28 07:39:00 +0530

    added frontend origin

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,9 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin:['https://eclypse-eight.vercel.app/','http://localhost:5173']
+}));
 app.use(express.json());
 app.use(express.static('public'));
 
